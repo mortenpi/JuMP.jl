0f2eb4cc23788aa855e342b082e7ceb62705cd22
odow <o.dowson@gmail.com>
2023-05-25 14:31:30 +1200

    Add CDDLib example

--- docs/src/tutorials/conic/arbitrary_precision.jl
+++ docs/src/tutorials/conic/arbitrary_precision.jl
@@ -11,9 +11,10 @@
 # This tutorial uses the following packages:
 
 using JuMP
+import CDDLib
 import Clarabel
 
-# ## Creating a model
+# ## Higher-precision arithmetic
 
 # To create a model with a number type other than `Float64`, use
 # [`GenericModel{T}`](@ref) with an optimizer which supports the same number
@@ -23,8 +24,6 @@ model = GenericModel{BigFloat}(Clarabel.Optimizer{BigFloat})
 
 # !!! tip
 #     `Model()` is short-hand for `GenericModel{Float64}()`.
-
-# ## Adding decision variables
 
 # The syntax for adding decision variables is the same as a normal JuMP
 # model, except that values are converted to `BigFloat`:
@@ -37,22 +36,25 @@ model = GenericModel{BigFloat}(Clarabel.Optimizer{BigFloat})
 # The lower and upper bounds of the decision variables are also `BigFloat`:
 
 lower_bound(x[1])
+
 #-
+
 typeof(lower_bound(x[1]))
 
 #-
-upper_bound(x[2])
-#-
-typeof(upper_bound(x[2]))
 
-# ## Adding constraints
+upper_bound(x[2])
+
+#-
+
+typeof(upper_bound(x[2]))
 
 # The syntax for adding constraints is the same as a normal JuMP model, except
 # that coefficients are converted to `BigFloat`:
 
 @constraint(model, c, x[1] == big"2" * x[2])
 
-# The function is a [`GenericaAffExpr`](@ref) with `BigFloat` for the
+# The function is a [`GenericAffExpr`](@ref) with `BigFloat` for the
 # coefficient and variable types;
 
 constraint = constraint_object(c)
@@ -62,8 +64,6 @@ typeof(constraint.func)
 
 typeof(constraint.set)
 
-# ## Adding an objective
-
 # The syntax for adding and objective is the same as a normal JuMP model, except
 # that coefficients are converted to `BigFloat`:
 
@@ -72,8 +72,6 @@ typeof(constraint.set)
 #-
 
 typeof(objective_function(model))
-
-# ## Solving and inspecting the solution
 
 # Here's the model we have built:
 
@@ -107,3 +105,56 @@ set_attribute(model, "tol_gap_abs", 1e-32)
 set_attribute(model, "tol_gap_rel", 1e-32)
 optimize!(model)
 value.(x) .- [3//7, 3//14]
+
+# ## Rational arithmetic
+
+# In addition to higher-precision floating point number types like `BigFloat`,
+# JuMP also supports solvers with exact rational arithmetic. One example is
+# CDDLib.jl, which supports the `Rational{BigInt}` number type:
+
+model = GenericModel{Rational{BigInt}}(CDDLib.Optimizer{Rational{BigInt}})
+
+# As before, we can create variables using rational bounds:
+
+@variable(model, 1 // 7 <= x[1:2] <= 2 // 3)
+
+#-
+
+lower_bound(x[1])
+
+#-
+
+typeof(lower_bound(x[1]))
+
+# As well as constraints:
+
+@constraint(model, c1, (2 // 1) * x[1] + x[2] <= 1)
+
+#-
+
+@constraint(model, c2, x[1] + 3x[2] <= 9 // 4)
+
+# and objective functions:
+
+@objective(model, Max, sum(x))
+
+# Here's the model we have built:
+
+print(model)
+
+# Let's solve and inspect the solution:
+
+optimize!(model)
+solution_summary(model)
+
+# The optimal values are given in exact rational arithmetic:
+
+value.(x)
+
+#-
+
+objective_value(model)
+
+#-
+
+value(c2)
